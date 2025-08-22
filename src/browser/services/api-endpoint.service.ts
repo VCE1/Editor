8857181d31971f87cc33ed747faa0ecffb8280c6
vce1 <1615910125qq@gmail.com>
2025-08-22 13:18:58 +0800

    Configure Vercel deployment with proxy and environment variables

--- src/browser/services/api-endpoint.service.ts
+++ src/browser/services/api-endpoint.service.ts
@@ -15,6 +15,6 @@ export class ApiEndpointService {
         return this.makePath('https://api.github.com', path);
     }
     ehTagConnectorDb(path = ''): string {
-        return this.makePath('https://ehtt.fly.dev/database', path);
+        return this.makePath('/api/database', path);
     }
 }
